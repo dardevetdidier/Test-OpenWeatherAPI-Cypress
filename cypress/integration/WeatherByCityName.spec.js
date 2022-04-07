3646303571b7edef3829a7907812ca3b941e27d9
Didier Dardevet <didier.dardevet@gmail.com>
2022-04-07 21:30:42 +0200

    Add fixtures

--- cypress/integration/WeatherByCityName.spec.js
+++ cypress/integration/WeatherByCityName.spec.js
@@ -1,21 +1,24 @@
 /// <reference types="Cypress" />
 
 describe("Open Weather API - By city Name suite tests", () => {
+
+    let cities = require("../fixtures/citiesData");
+
     it("Should return Code 200 with valid city name", () => {
-        cy.request({
-            url: "https://api.openweathermap.org/data/2.5/weather",
-            qs: {
-                q: "Paris",
-                units: "metric",
-                appid: Cypress.env('API_KEY')
-            }
-        }).then((response) => {
+        cy.RequestByCityName(cities[0].name).then((response) => {
             expect(response.status).to.eql(200);
         })
     })
 
+    it("Should return the right city name", () => {
+        cy.RequestByCityName(cities[1].name)
+        .then((response) => {
+            expect(response.body.name).to.eql(cities[1].name);
+        })
+    })
+
     it("Should return valid schema", () => {
-        cy.RequestByCityName("Paris")
+        cy.RequestByCityName(cities[1].name)
         .then((response) => {
             expect(response.body).have.property("coord").to.be.an('object');
             expect(response.body).have.property("weather").to.be.an('array');
