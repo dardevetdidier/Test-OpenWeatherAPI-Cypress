1f9f64bf39f63f3dd88826dbcb7408b29537258e
Didier Dardevet <didier.dardevet@gmail.com>
2022-04-09 20:16:59 +0200

    Merge branch 'main' of https://github.com/dardevetdidier/Test-OpenWeatherAPI-Cypress

--- cypress/integration/WeatherByCityName.spec.js
+++ cypress/integration/WeatherByCityName.spec.js
@@ -1,30 +1,45 @@
 /// <reference types="Cypress" />
 
 describe("Open Weather API - By city Name suite tests", () => {
-
     let cities = require("../fixtures/citiesData");
 
+    function formatCityName(index) {
+        if (cities[index].name.includes(",")) {
+            return cities[index].name.split(", ")[0];
+        } else {
+            return cities[index].name;
+        }
+    }
+
     it("Should return Code 200 with valid city name", () => {
-        cy.RequestByCityName(cities[0].name).then((response) => {
-            expect(response.status).to.eql(200);
-        })
-    })
+        for (let i = 0; i < cities.length; i++) {
+            cy.RequestByCityName(cities[i].name).then((response) => {
+                expect(response.status).to.eq(200);
+            });
+        }
+    });
 
     it("Should return the right city name", () => {
-        cy.RequestByCityName(cities[1].name)
-        .then((response) => {
-            expect(response.body.name).to.eql(cities[1].name);
-        })
-    })
+        for (let i = 0; i < cities.length; i++) {
+            cy.RequestByCityName(cities[i].name).then((response) => {
+                expect(response.body.name).to.eql(formatCityName(i));
+            });
+        }
+    });
 
-    it("Should return valid schema", () => {
-        cy.RequestByCityName(cities[1].name)
-        .then((response) => {
-            expect(response.body).have.property("coord").to.be.an('object');
-            expect(response.body).have.property("weather").to.be.an('array');
-            expect(response.body).have.property("wind").to.be.an('object');
-            expect(response.body).have.property("clouds").to.be.an('object');
-            expect(response.body).have.property("name").to.be.an('string');
-        })
-    })
-})
+    it("Should return a valid schema", () => {
+        for (let i = 0; i < cities.length; i++) {
+            cy.RequestByCityName(cities[i].name).then((response) => {
+                expect(response.body).have.property("coord").to.be.an("object");
+                expect(response.body)
+                    .have.property("weather")
+                    .to.be.an("array");
+                expect(response.body).have.property("wind").to.be.an("object");
+                expect(response.body)
+                    .have.property("clouds")
+                    .to.be.an("object");
+                expect(response.body).have.property("name").to.be.a("string");
+            });
+        }
+    });
+});
